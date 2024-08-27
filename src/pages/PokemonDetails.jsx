fd8227d14d6c3b6cf6895d0e7d63c2d4cb7d2021
JonathanQ77 <jonathan.quesnel91@gmail.com>
2024-08-27 23:48:55 +0200

    animation avec framer motion

--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -8,6 +8,7 @@ import { toast } from "react-toastify";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { usePureFetch } from "../hooks/usePureFetch";
 import { queryClient } from "../utils/query";
+import { motion, AnimatePresence } from "framer-motion";
 //https://believemy-pokedex-ce1dd-default-rtdb.europe-west1.firebasedatabase.app/pokemons.json
 export default function PokemonDetails() {
   // Variables
@@ -255,7 +256,6 @@ export default function PokemonDetails() {
   return (
     <div className="max-w-7xl mx-auto">
       <Pokecard pokemon={pokemon} details />
-
       {isNaN(id) && (
         <div className="flex justify-end gap-4 mt-5">
           <div
@@ -272,56 +272,73 @@ export default function PokemonDetails() {
           </div>
         </div>
       )}
+      {createPortal(
+        <AnimatePresence>
+          {updatePokemon && (
+            <div className="bg-black bg-opacity-90 fixed top-0 right-0 bottom-0 left-0 flex justify-center text-black">
+              <motion.div
+                variants={{
+                  hidden: {
+                    opacity: 0,
+                    y: 30,
+                  },
+                  visible: { opacity: 1, y: 0 },
+                }}
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                transition={{
+                  duration: 0.4,
+                }}
+                className="p-8 bg-white cursor-auto rounded-xl max-w-xl w-full my-10 overflow-y-auto"
+              >
+                {/* Close */}
+                <div className="flex justify-end text-black hover:text-yellow-pokemon cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-8 w-8"
+                    viewBox="0 0 24 24"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setUpdatePokemon(false);
+                    }}
+                  >
+                    <g fill="currentColor">
+                      <path d="M10.03 8.97a.75.75 0 0 0-1.06 1.06L10.94 12l-1.97 1.97a.75.75 0 1 0 1.06 1.06L12 13.06l1.97 1.97a.75.75 0 0 0 1.06-1.06L13.06 12l1.97-1.97a.75.75 0 1 0-1.06-1.06L12 10.94l-1.97-1.97Z"></path>
+                      <path
+                        fillRule="evenodd"
+                        d="M12.057 1.25h-.114c-2.309 0-4.118 0-5.53.19c-1.444.194-2.584.6-3.479 1.494c-.895.895-1.3 2.035-1.494 3.48c-.19 1.411-.19 3.22-.19 5.529v.114c0 2.309 0 4.118.19 5.53c.194 1.444.6 2.584 1.494 3.479c.895.895 2.035 1.3 3.48 1.494c1.411.19 3.22.19 5.529.19h.114c2.309 0 4.118 0 5.53-.19c1.444-.194 2.584-.6 3.479-1.494c.895-.895 1.3-2.035 1.494-3.48c.19-1.411.19-3.22.19-5.529v-.114c0-2.309 0-4.118-.19-5.53c-.194-1.444-.6-2.584-1.494-3.479c-.895-.895-2.035-1.3-3.48-1.494c-1.411-.19-3.22-.19-5.529-.19ZM3.995 3.995c.57-.57 1.34-.897 2.619-1.069c1.3-.174 3.008-.176 5.386-.176s4.086.002 5.386.176c1.279.172 2.05.5 2.62 1.069c.569.57.896 1.34 1.068 2.619c.174 1.3.176 3.008.176 5.386s-.002 4.086-.176 5.386c-.172 1.279-.5 2.05-1.069 2.62c-.57.569-1.34.896-2.619 1.068c-1.3.174-3.008.176-5.386.176s-4.086-.002-5.386-.176c-1.279-.172-2.05-.5-2.62-1.069c-.569-.57-.896-1.34-1.068-2.619c-.174-1.3-.176-3.008-.176-5.386s.002-4.086.176-5.386c.172-1.279.5-2.05 1.069-2.62Z"
+                        clipRule="evenodd"
+                      ></path>
+                    </g>
+                  </svg>
+                </div>
 
-      {updatePokemon &&
-        createPortal(
-          <div className="bg-black bg-opacity-90 fixed top-0 right-0 bottom-0 left-0 flex justify-center text-black">
-            <div className="p-8 bg-white cursor-auto rounded-xl max-w-xl w-full my-10 overflow-y-auto">
-              {/* Close */}
-              <div className="flex justify-end text-black hover:text-yellow-pokemon cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-8 w-8"
-                  viewBox="0 0 24 24"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setUpdatePokemon(false);
-                  }}
-                >
-                  <g fill="currentColor">
-                    <path d="M10.03 8.97a.75.75 0 0 0-1.06 1.06L10.94 12l-1.97 1.97a.75.75 0 1 0 1.06 1.06L12 13.06l1.97 1.97a.75.75 0 0 0 1.06-1.06L13.06 12l1.97-1.97a.75.75 0 1 0-1.06-1.06L12 10.94l-1.97-1.97Z"></path>
-                    <path
-                      fillRule="evenodd"
-                      d="M12.057 1.25h-.114c-2.309 0-4.118 0-5.53.19c-1.444.194-2.584.6-3.479 1.494c-.895.895-1.3 2.035-1.494 3.48c-.19 1.411-.19 3.22-.19 5.529v.114c0 2.309 0 4.118.19 5.53c.194 1.444.6 2.584 1.494 3.479c.895.895 2.035 1.3 3.48 1.494c1.411.19 3.22.19 5.529.19h.114c2.309 0 4.118 0 5.53-.19c1.444-.194 2.584-.6 3.479-1.494c.895-.895 1.3-2.035 1.494-3.48c.19-1.411.19-3.22.19-5.529v-.114c0-2.309 0-4.118-.19-5.53c-.194-1.444-.6-2.584-1.494-3.479c-.895-.895-2.035-1.3-3.48-1.494c-1.411-.19-3.22-.19-5.529-.19ZM3.995 3.995c.57-.57 1.34-.897 2.619-1.069c1.3-.174 3.008-.176 5.386-.176s4.086.002 5.386.176c1.279.172 2.05.5 2.62 1.069c.569.57.896 1.34 1.068 2.619c.174 1.3.176 3.008.176 5.386s-.002 4.086-.176 5.386c-.172 1.279-.5 2.05-1.069 2.62c-.57.569-1.34.896-2.619 1.068c-1.3.174-3.008.176-5.386.176s-4.086-.002-5.386-.176c-1.279-.172-2.05-.5-2.62-1.069c-.569-.57-.896-1.34-1.068-2.619c-.174-1.3-.176-3.008-.176-5.386s.002-4.086.176-5.386c.172-1.279.5-2.05 1.069-2.62Z"
-                      clipRule="evenodd"
-                    ></path>
-                  </g>
-                </svg>
-              </div>
+                <h2 className="text-3xl font-semibold mb-5">
+                  Modifier un pokémon
+                </h2>
 
-              <h2 className="text-3xl font-semibold mb-5">
-                Modifier un pokémon
-              </h2>
-
-              <MakeForm // FORM DE MISE A JOUR POKEMON
-                name={name}
-                height={height}
-                weight={weight}
-                hp={hp}
-                attack={attack}
-                defense={defense}
-                specialAttack={specialAttack}
-                specialDefense={specialDefense}
-                speed={speed}
-                image={image}
-                types={types}
-                onFormSubmittedHandler={beforeMutate}
-                pokemon={pokemon}
-              />
+                <MakeForm // FORM DE MISE A JOUR POKEMON
+                  name={name}
+                  height={height}
+                  weight={weight}
+                  hp={hp}
+                  attack={attack}
+                  defense={defense}
+                  specialAttack={specialAttack}
+                  specialDefense={specialDefense}
+                  speed={speed}
+                  image={image}
+                  types={types}
+                  onFormSubmittedHandler={beforeMutate}
+                  pokemon={pokemon}
+                />
+              </motion.div>
             </div>
-          </div>,
-          document.body
-        )}
+          )}
+        </AnimatePresence>,
+        document.body
+      )}
     </div>
   );
 }
